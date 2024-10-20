062ace318f6982fa0744a9c9aab53d40efb0d131
PriyabrataNaik <priyabratanaik330@gmail.com>
2024-10-20 11:07:41 +0530

    second commit -> Handled the Home view top bar UI element

--- src/main/java/com/example/secondwishlistapp/AppBar.kt
+++ src/main/java/com/example/secondwishlistapp/AppBar.kt
@@ -2,6 +2,8 @@ package com.example.secondwishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
@@ -44,6 +46,8 @@ fun AppBarView(
                     .heightIn(max = 24.dp)
             )
         },
+        modifier = Modifier
+            .statusBarsPadding(),
         elevation = 3.dp,
         backgroundColor = colorResource(id = R.color.app_bar_color),
         navigationIcon = navigationIcon
